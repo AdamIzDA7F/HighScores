3672e8ffd801ad3e7d905aac7ad5989c11e4f692
ADAM MCDERMOTT <am0894132@otc.edu>
2024-06-11 22:09:53 +0000

    Added 4 files to /

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+
+namespace HighScores
+{
+
+    class PlayerScore
+    {
+        private int score;
+        public int Score
+        {
+            get { return score; }
+            set
+            {
+                if (value >= 0)
+                {
+                    score = value;
+                }
+                else
+                {
+                  Console.WriteLine("Enter a positive score.");
+                  score = 0;
+                }
+            }
+        }
+    }
+
+    class Players
+    {
+   
+        private string initials;
+        public string Initials
+        {
+            get { return initials; }
+            set
+            {
+                if (value.Length <= 3)
+                {
+                    initials = value.ToUpper();
+                }
+                else
+                {
+                Console.WriteLine("Initials should be 3 characters or less.");
+                }
+            }
+        }
+    }
+
+    class Program
+        {
+            static void Main(string[] args)
+            {
+         
+            Players player = new Players();
+            Console.WriteLine("Enter Player Initials (up to 3 characters):");
+           string Initials = Console.ReadLine();
+            player.Initials = Initials;
+            Console.WriteLine("Player's Initials: " + player.Initials);
+
+            PlayerScore score = new PlayerScore();
+            Console.Write("Enter your score: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int scores))
+            {
+             score.Score = scores;
+             Console.WriteLine("New High Score: " + score.Score);
+            }
+            else
+            {
+            Console.WriteLine("Invalid score. Please enter a valid score. No Negatives");
+            }
+        }
+    }
+}
